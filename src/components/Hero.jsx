cf739060116cd4208de57659fa5b4743c727cd6c
edward krisna <edwardsembiring1000@gmail.com>
2026-01-20 00:14:17 +0700

    update home

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,18 +1,24 @@
 const Hero = () => {
   return (
-    <div className="relative overflow-hidden w-[52vh] md:w-full h-[80vh] md:h-screen mx-auto">
+    <div className="relative overflow-hidden w-[61.5vh] md:w-full h-[80vh] md:h-screen mx-auto">
       <div className="relative w-full h-full overflow-hidden">
         <picture>
-          {/* Mobile */}
+          {/* Mobile image */}
           <source
             media="(max-width: 768px)"
             srcSet="/img/hero-mobile.JPG"
           />
-          {/* Desktop */}
+
+          {/* Desktop + Mobile renderer */}
           <img
             src="/img/hero.JPG"
             alt="Corporate Planning 2026"
-            className="absolute inset-0 w-full h-full object-cover object-[60%_center] md:object-center"/>
+            className="
+              absolute inset-0 w-full h-full object-cover
+              object-center            /* mobile: always center */
+              md:object-center         /* desktop center */
+            "
+          />
         </picture>
       </div>
     </div>
